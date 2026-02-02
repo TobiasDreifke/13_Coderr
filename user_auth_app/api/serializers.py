f62e27b2f12d15d5f4df0488c7eb58d90b236ff4
Tobias Dreifke <toobsecret@gmail.com>
2026-02-02 12:27:57 +0100

    feat: add Registration and Login Serializer

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -1,22 +1,74 @@
 from rest_framework import serializers
+from ..models import UserProfile
 from django.contrib.auth.models import User
 
 
-# Example User Serializer
-class UserSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'email', 'password', 'repeated_password', 'type']
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+    type = serializers.ChoiceField(
+        choices=UserProfile.TYPE_CHOICES, write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        
+        fields = ['username', 'email', 'password', 'repeated_password', 'type']
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
 
-# Example Registration Serializer
-# class RegisterSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(write_only=True)
-#     
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password']
-#     
-#     def create(self, validated_data):
-#         user = User.objects.create_user(**validated_data)
-#         return user
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email already exists')
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['repeated_password']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Username already exists')
+        return value
+
+    def save(self):
+        pw = self.validated_data['password']
+        user = User(
+            username=self.validated_data['username'],
+            email=self.validated_data['email']
+        )
+        user.set_password(pw)
+        user.save()
+        UserProfile.objects.create(
+            username=user,
+            type=self.validated_data.get('type')
+        )
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Invalid username or password")
+        
+        if not user.check_password(password):
+            raise serializers.ValidationError("Invalid username or password")
+        
+        data['user'] = user
+        return data
+      
