cddbafe108ddde224f3275530703d1417f0c9768
Tobias Dreifke <toobsecret@gmail.com>
2026-02-02 12:27:09 +0100

    first commit

---
+++ orders_app/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Add your API endpoints here
+    # Example:
+    # path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+]
