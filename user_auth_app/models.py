05acdac508a7f512be0bfc037c7e0368499bf28e
Tobias Dreifke <toobsecret@gmail.com>
2026-02-02 12:27:31 +0100

    feat: add CustomUserProfile

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    TYPE_CHOICES = [
+        ('customer', 'Customer'),
+        ('business', 'Business'),
+    ]
+
+    username = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    type = models.CharField(
+        max_length=50, choices=TYPE_CHOICES, default='customer')
