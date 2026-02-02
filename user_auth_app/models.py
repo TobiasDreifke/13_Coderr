8feca58a84c694ba96b5149305fc54bb23aa8582
Tobias Dreifke <toobsecret@gmail.com>
2026-02-02 13:33:23 +0100

    fix: extendet user model

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
 class UserProfile(models.Model):
     TYPE_CHOICES = [
         ('customer', 'Customer'),
@@ -14,3 +11,14 @@ class UserProfile(models.Model):
         User, on_delete=models.CASCADE, related_name='profile')
     type = models.CharField(
         max_length=50, choices=TYPE_CHOICES, default='customer')
+    
+    
+    file = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    tel = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    working_hours = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username.username}'s profile"
