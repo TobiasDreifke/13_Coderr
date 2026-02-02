5c168bb376f7f025c2167d5fc1e832e2ab4ac824
Tobias Dreifke <toobsecret@gmail.com>
2026-02-02 15:24:42 +0100

    feat: add offerdetailserializer

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -1,22 +1,11 @@
+
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from offers_app.models import  OfferDetail
 
 
-# Example User Serializer
-class UserSerializer(serializers.ModelSerializer):
+class OfferDetailSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        
-
-# Example Registration Serializer
-# class RegisterSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(write_only=True)
-#     
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password']
-#     
-#     def create(self, validated_data):
-#         user = User.objects.create_user(**validated_data)
-#         return user
+        model = OfferDetail
+        fields = ['id', 'title', 'revisions', 'delivery_time_in_days',
+                  'price', 'features', 'offer_type'
+                  ]
