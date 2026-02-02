7d5d53696fed7bd6abf732fe8aa64de412eab60d
Tobias Dreifke <toobsecret@gmail.com>
2026-02-02 15:25:00 +0100

    feat: add offer detail view

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,28 +1,10 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserSerializer
+from rest_framework import viewsets, status, permissions, generics
+from ..models import Offer, OfferDetail
+from .serializers import (
+    OfferDetailSerializer
+)
 
-
-# Example API view
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def user_profile(request):
-    """Get the current user's profile."""
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
-
-
-# Example Registration View
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# def register(request):
-#     serializer = RegisterSerializer(data=request.data)
-#     if serializer.is_valid():
-#         user = serializer.save()
-#         return Response({
-#             'user': UserSerializer(user).data,
-#             'message': 'User registered successfully'
-#         }, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class OfferDetailViewSet(generics.ListCreateAPIView):
+    queryset = OfferDetail.objects.all()
+    serializer_class = OfferDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
