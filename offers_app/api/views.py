71653059f2493d8eb819319a9b96b4a83c98b654
Tobias Dreifke <toobsecret@gmail.com>
2026-02-13 10:44:05 +0100

    feat: add offer views

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,10 +1,26 @@
-from rest_framework import viewsets, status, permissions, generics
+from rest_framework import permissions, generics
 from ..models import OfferDetail, Offer
 from .serializers import (
-    OfferDetailSerializer
+    OfferDetailSerializer, OfferSerializer, OfferListSerializer, OfferRetrieveSerializer
 )
 
-class OfferDetailViewSet(generics.ListCreateAPIView):
+
+class OfferRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Offer.objects.all().prefetch_related('details')
+    serializer_class = OfferRetrieveSerializer  
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class OfferListCreateView(generics.ListCreateAPIView):
+    queryset = Offer.objects.all().prefetch_related('details')
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OfferSerializer    
+        return OfferListSerializer
+
+class OfferDetailRetrieveView(generics.RetrieveAPIView):
     queryset = OfferDetail.objects.all()
-    serializer_class = OfferDetailSerializer
+    serializer_class = OfferDetailSerializer  
     permission_classes = [permissions.IsAuthenticated]
