c613f08142fefc62d5a6f0ccf6596ded73b88c71
Tobias Dreifke <toobsecret@gmail.com>
2026-02-16 12:45:00 +0100

    feat: add order serializers

--- orders_app/api/serializers.py
+++ orders_app/api/serializers.py
@@ -1,22 +1,65 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from ..models import Order
+from offers_app.models import OfferDetail
 
 
-# Example User Serializer
-class UserSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        
+        model = Order
+        fields = [
+            'id',
+            'customer_user',
+            'business_user',
+            'title',
+            'revisions',
+            'delivery_time_in_days',
+            'price',
+            'features',
+            'offer_type',
+            'status',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = [
+            'id', 'customer_user', 'business_user',
+            'title', 'revisions', 'delivery_time_in_days',
+            'price', 'features', 'offer_type',
+            'status', 'created_at', 'updated_at'
+        ]
 
-# Example Registration Serializer
-# class RegisterSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(write_only=True)
-#     
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password']
-#     
-#     def create(self, validated_data):
-#         user = User.objects.create_user(**validated_data)
-#         return user
+
+class OrderUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['status']
+
+
+class OrderCreateSerializer(serializers.Serializer):
+    offer_detail_id = serializers.IntegerField()
+
+    def validate_offer_detail_id(self, value):
+        try:
+            OfferDetail.objects.get(id=value)
+        except OfferDetail.DoesNotExist:
+            raise serializers.ValidationError("OfferDetail nicht gefunden")
+        return value
+
+    def create(self, validated_data):
+        offer_detail_id = validated_data['offer_detail_id']
+
+        offer_detail = OfferDetail.objects.select_related('offer__user').get(
+            id=offer_detail_id
+        )
+
+        order = Order.objects.create(
+            customer_user=self.context['request'].user,
+            business_user=offer_detail.offer.user,
+            title=offer_detail.title,
+            revisions=offer_detail.revisions,
+            delivery_time_in_days=offer_detail.delivery_time_in_days,
+            price=offer_detail.price,
+            features=offer_detail.features,
+            offer_type=offer_detail.offer_type,
+        )
+
+        return order
