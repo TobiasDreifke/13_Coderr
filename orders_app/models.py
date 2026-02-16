43c78ff3b8f9a0ceacff980bd7428e65ab9f5812
Tobias Dreifke <toobsecret@gmail.com>
2026-02-16 10:18:20 +0100

    feat: add Order model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Order(models.Model): 
+
+    STATUS_CHOICES = [
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    OFFER_TYPE_CHOICES = [
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium'),
+    ]
+
+    customer_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders_as_customer'
+    )
+    business_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders_as_business'
+    )
+
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField()
+    offer_type = models.CharField(max_length=10, choices=OFFER_TYPE_CHOICES)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='in_progress'  
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.title}"
