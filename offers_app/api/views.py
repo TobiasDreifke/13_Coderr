1c0481c5c127dbc91d1616b294ddf90c48dfcf26
Tobias Dreifke <toobsecret@gmail.com>
2026-02-23 10:12:29 +0100

    fix: pagination set to single view instead of global

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,14 +1,23 @@
 from rest_framework import permissions, generics
 from ..models import OfferDetail, Offer
 from .serializers import (
-    OfferDetailSerializer, OfferSerializer, 
+    OfferDetailSerializer, OfferSerializer,
     OfferListSerializer, OfferRetrieveSerializer
 )
 from .permissions import IsBusinessUser, IsOwner
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_query_param = 'p'
 
 
 class OfferListCreateView(generics.ListCreateAPIView):
     queryset = Offer.objects.all().prefetch_related('details')
+    pagination_class = LargeResultsSetPagination
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
