cd089b4af9d0aaa0219f06b3aac3e03a51b26ffb
Tobias Dreifke <toobsecret@gmail.com>
2026-02-23 14:02:56 +0100

    erfactor: clean up

--- orders_app/api/serializers.py
+++ orders_app/api/serializers.py
@@ -32,11 +32,9 @@ class OrderUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'customer_user', 'business_user',
-            'title', 'revisions', 'delivery_time_in_days',
-            'price', 'features', 'offer_type',
-            'status', 'created_at', 'updated_at']
-
-
+                  'title', 'revisions', 'delivery_time_in_days',
+                  'price', 'features', 'offer_type',
+                  'status', 'created_at', 'updated_at']
 class OrderCreateSerializer(serializers.Serializer):
     offer_detail_id = serializers.IntegerField()
 
