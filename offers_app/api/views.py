e92fd8e9bc036987492b773c90c45968744cfa98
Tobias Dreifke <toobsecret@gmail.com>
2026-04-13 12:16:45 +0200

    fix: wrong query parameter

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -15,7 +15,7 @@ class LargeResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
-    page_query_param = 'p'
+    page_query_param = 'page'
 
 
 class OfferListCreateView(generics.ListCreateAPIView):
